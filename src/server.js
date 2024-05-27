031566df639804217e1f9b3b31de6b2d4354cf44
nabanichowdhury <nabanichowdhury@gmail.com>
2024-05-27 08:33:05 +0600

    commiting changes

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
+import "dotenv/config";
+// require("dotenv").config();
 
 import app from "./index.js";
 const port = 7001;
-const uri =
-  "mongodb+srv://dbadmin:lashmRyT6EP6ESL8@cluster0.vdcw4ws.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@cluster0.vdcw4ws.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
